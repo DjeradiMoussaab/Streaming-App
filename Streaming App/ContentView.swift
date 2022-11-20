4d3d59db06f910c5e7fea8e1344d84eec865553d
Moussaab Djeradi <fm_djeradi@esi.dz>
2022-11-20 08:25:12 +0100

    Initial Commit

---
+++ Streaming App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Streaming App
+//
+//  Created by Moussaab Djeradi on 20/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Streaming App/Streaming_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Streaming_AppApp.swift
+//  Streaming App
+//
+//  Created by Moussaab Djeradi on 20/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Streaming_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
